9ed75636f43c04a5676b8f6d5bab0d30a5261bb6
oldpondplop <>
2025-01-27 13:26:30 +0200

    pass session to db

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,29 +1,38 @@
 from sqlmodel import Session, create_engine, SQLModel, select
 from app.core.config import settings
-from app.models import RoleEnum, User, UserCreate
+from app.models import User, UserCreate
 from app import crud
 
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI),  connect_args={"check_same_thread": False}, echo=True)
+engine = create_engine(
+    str(settings.SQLALCHEMY_DATABASE_URI),
+    connect_args={"check_same_thread": False}, 
+    echo=True
+)
 
-def init_db() -> None:
+def init_db(session: Session| None = None) -> None:
+    """Initialize the database."""
+    if session is None:  
+        session = Session(engine)  
+
     SQLModel.metadata.create_all(engine)
-    
-    with Session(engine) as session:
-        admin: User | None = session.exec(select(User).where(User.email == settings.FIRST_SUPERUSER)).first()
-        if not admin:
-            admin_in = UserCreate(
-                user_id="ADMIN001",
-                name="Admin",
-                email=settings.FIRST_SUPERUSER,
-                password=settings.FIRST_SUPERUSER_PASSWORD,
-                role_name=RoleEnum.ADMIN.value,
-                is_superuser=True,
-                is_active=True,
-            )
-            admin = crud.create_user(session=session, user_create=admin_in)
 
-    print("Database initialized successfully!")
+    admin = session.exec(select(User).where(User.email == settings.FIRST_SUPERUSER)).first()
+    if not admin:
+        admin_in = UserCreate(
+            user_id="ADMIN001",
+            name="Admin",
+            role_name="admin",
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+            is_active=True,
+        )
+        admin = crud.create_user(session=session, user_create=admin_in)
+
+    session.commit()  
+    print("✅ Database initialized successfully!")
 
 
 if __name__ == "__main__":
-    init_db()
+    with Session(engine) as session:
+        init_db(session)
