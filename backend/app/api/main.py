542cb63261b0084fab1fea6f4a300cc6ed45caef
oldpondplop <>
2025-02-01 22:07:06 +0200

    add quizz router, delete items

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, private, users, utils, courses, roles
+from app.api.routes import login, private, quizzes, users, utils, courses, roles
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -9,6 +9,7 @@ api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(roles.router)
 api_router.include_router(courses.router)
+api_router.include_router(quizzes.router)
 
 
 if settings.ENVIRONMENT == "local":
